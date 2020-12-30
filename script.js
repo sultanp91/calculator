f391ecb5ff8adaf375aba76d4b41f90cb7bcd032
Sultan <sultanp@gmail.com>
2020-12-30 13:56:36 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+const multiply = (a, b ) => {
+    return a * b;
+}
+
+const divide = (a, b) => {
+    return a / b;
+}
+
+const operate = (a ,b, operator) => {
+    return operator(a, b);
+}
+
