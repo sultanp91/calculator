83c307ec09c863810691fbd687ba6c53a9be58d8
Sultan <sultanp@gmail.com>
2020-12-31 00:38:57 +0000

    addition function is working

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Operators to be used inside the calculator
+
 const add = (a, b) => {
     return a + b;
 }
@@ -14,7 +16,56 @@ const divide = (a, b) => {
     return a / b;
 }
 
-const operate = (a ,b, operator) => {
-    return operator(a, b);
+let operand;
+
+// operand will store the current selection for which operator to use
+
+let operate = (a, b, c) => {
+    return c(a, b);
 }
 
+// operate can take in 
+
+let storedValue = [];
+let currentValue = [];
+
+let input = document.querySelector(".input");
+let displayNum  = document.querySelector("#display");
+let plusButton = document.querySelector("#plus");
+let minusButton = document.querySelector("#minus");
+let multiplyButton = document.querySelector("#multiply");
+let divideButton = document.querySelector("#divide")
+let equalsButton = document.querySelector("#equals");
+
+let updateDisplay = () =>{
+let newDisplay = parseInt(currentValue.join(""))
+displayNum.textContent = newDisplay;
+}
+
+let storeValue = () => {
+    storedValue = parseInt([...currentValue].join(""));
+    currentValue.splice(0, currentValue.length);
+}
+
+input.addEventListener("click", (e) => {
+    if(e.target.className === "numinput" || e.target.parentElement.className === "numinput"){
+
+        currentValue.push(e.target.textContent);
+
+        updateDisplay();
+    }
+});
+
+
+plusButton.addEventListener("click", () => {
+    storeValue();
+    operand = add;
+})
+
+ equalsButton.addEventListener("click", () => {
+
+        let result = operate(storedValue, parseInt(currentValue.join("")), operand)
+
+        storedValue = result;
+        displayNum.textContent = result;
+ });
